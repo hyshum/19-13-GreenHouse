e19174ebf1b3fdf607e31d12d01fb2ca5ad66dd6
odorencz <odorencz@bu.edu>
2019-02-12 16:30:01 -0500

    Serial Connection + Wifi
    Send data over serial connection between Arduino and NodeMCU. Can also then send to web app on phone. LEDs are used for debugging, will be removed

--- arduino/test_wifi/node_mcu/node_mcu.ino
+++ arduino/test_wifi/node_mcu/node_mcu.ino
@@ -20,20 +20,19 @@ dht DHT;
 #define DHT11_PIN D1 //Nodemcu: D1, Arduino: 4
 #define LIGHT_PIN D3  //Nodemcu: D2, Arduino: 0
 #define OUTSIDE_TEMP_PIN A0  //Nodemcu: D5, Arduino: 1
-#define RW_PIN D0
+#define WRITE_PIN D0
+#define READ_PIN D5
 #define LED_PIN D1
+#define WIFI_ON D2
 //#define Soil_moisture_PIN A0  //Nodemcu: D7, Arduino: 1
 int in1 = D6;  //Nodemcu: D6, Arduino: 7
 
-
-int out_reading;
-float voltage;
 float tempOutF;
-int chk;
 double temp;
 double humidity;
 int on;
 int photoCellReading;
+bool changeTemp = false;
 
 unsigned long StartTime;
 unsigned long FinishTime;
@@ -52,214 +51,24 @@ int sensorValue = 0;
 int percent = 0;
 String percentString ="0";
 int stringLength = 0;
-
-void handleRoot() {
-String html = "<!DOCTYPE html>";
-html += "<html lang=\"en\">";
-html += "<head> <meta charset=\"utf-8\"> <meta http-equiv=\"refresh\" content=\"10\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> </head>";
-html += "<body>";
-html += "<h1>Sensor Data</h1>";
-if (Warning_temperture_low) {
-  html += "<p>Warning: temperature is too low</p>";
-}
-if (humidity>90) {
-  html += "<p>Warning: humidity is too high</p>";
-}
-if (humidity<30) {
-  html += "<p>Warning: humidity is too low</p>";
-}
-/*if( percent <10)
-{
-  html += "<p>Warning: soil moisture is too low</p>";
-}*/
-html += "<p>Temperature: </p>";
-html += temp;
-html += "<br><br>";
-html += "<p>Humidity: </p>";
-html += humidity;
-html += "<br><br>";
-html += "<p>Temperature outside</p>";
-html += tempOutF;
-//html += "<p>Soil Mositure: </p>";
-//html += percentString;
-html += "<br><br>";
-html += "<h1>Input the threshold temperature</h1>";
-html += "<input type='text' name='date_hh' id='date_hh' size=2 autofocus> Degree";
-html += "<div> <br><button id=\"save_button\">Submit</button></div>";
-html += "<br>";
-html += "<p>Heater running time(s): </p>";
-html += TotalRunningTime / 1000;
-html += "<br><br>";
-//html += "<div> <br><button onclick="document.getElementById('date_hh').value = ''" id=\"save_button\">Submit</button></div>";
-html += "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>"; 
-html += "<script>";
-html += "var hh;";
-html += "var mm;";
-html += "var ss;";
-html += "$('#save_button').click(function(e){";
-html += "e.preventDefault();";
-html += "hh = $('#date_hh').val();";
-html += "mm = $('#date_mm').val();";
-html += "ss = $('#date_ss').val();";        
-html += "$.get('/save?hh=' + hh + '&mm=' + mm + '&ss=' + ss, function(data){";
-html += "console.log(data);";
-html += "});";
-html += "})";     
-html += "</script>";
-html += "</body>";
-html += "</html>";
-
-
-   server.send ( 200, "text/html", html );  
-
-}
-
-
-void handleSave() {
-  if (server.arg("hh")!= ""){
-    threshold_temperature = server.arg("hh").toInt();
-  }
-}
-
-
-void setup() {
-
-  pinMode(RW_PIN, OUTPUT);
-  digitalWrite(RW_PIN, LOW);
-  pinMode( LED_PIN, OUTPUT );
-  digitalWrite( LED_PIN, LOW );
-  // Start serial
-  Serial.begin(115200);
-  //delay(10);
-
-  // Connecting to a WiFi network
-  /*Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  server.on ( "/", handleRoot );
-  server.on ("/save", handleSave);
-
-  server.begin();
-  Serial.println ( "HTTP server started" );*/
-
-
-}
-int incomingByte;      // a variable to read incoming serial data into
-String incomingString;
-bool changeTemp = 0;
-void loop() {
-
-
-    if( changeTemp == 1 )
-    {
-      digitalWrite( RW_PIN, HIGH );
-      delay( 5000 );
-      Serial.print( "hello" );
-      digitalWrite( LED_PIN, LOW );
-      changeTemp = 0;
-      delay( 5000 );
-    }
-    else
-    {
-      digitalWrite( RW_PIN, LOW );
-
-      delay( 5000 );
-      while( true )
-      {
-        if (Serial.available() > 0) {
-        
-          // read the oldest byte in the serial buffer:
-          incomingString = Serial.readString();
-          if( incomingString == "arduino" )
-          {
-            Serial.println( "woohoo!" );
-            digitalWrite( LED_PIN, HIGH );
-            changeTemp = 1; 
-            break;
-          }
-  
-        }
-      }
-    }
-      
-      //delay( 5000 );
-      /*
-      changeTemp = 1;
-    }
-    //changeTemp = ~changeTemp;
-   /*
-  server.handleClient();
-  Serial.print("The current threshold temperature is ");
-  Serial.println(threshold_temperature);
-  Serial.println("");
-  read_print_temperature();
-  
-      if (client.connect(thingspeakserver, 80)) //   "184.106.153.149" or api.thingspeak.com
-    {
-
-        String postStr = apiKey;
-        postStr += "&field1=";
-        postStr += String(temp);   //Temperature
-        postStr += "&field2=";
-        postStr += String(humidity);   //Humidity
-        postStr += "&field3=";
-        postStr += String(tempOutF);   //Humidity
-        postStr += "\r\n\r\n";
-
-        client.print("POST /update HTTP/1.1\n");
-        client.print("Host: api.thingspeak.com\n");
-        client.print("Connection: close\n");
-        client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
-        client.print("Content-Type: application/x-www-form-urlencoded\n");
-        client.print("Content-Length: ");
-        client.print(postStr.length());
-        client.print("\n\n");
-        client.print(postStr);
-    }
-    client.stop();
-
-    //sensorValue = analogRead(Soil_moisture_PIN);
-    //percent = convertToPercent(sensorValue);
-    //percentString = String(percent);
-    //Serial.println( "sensor val" + sensorValue );
-    //Serial.println( "percent: " + percentString );
-
-    delay(5000);
-}
-
-void read_print_temperature() {
+/*void read_sensors() {
    out_reading = analogRead( OUTSIDE_TEMP_PIN );
    voltage = out_reading * 3.3;
-  voltage /= 1024.0;
+    voltage /= 1024.0;
 
    tempOutF = (voltage - 0.5) * 100 * 9.0/5.0 + 29;
    chk = DHT.read11(DHT11_PIN);
    temp = DHT.temperature * 1.8 + 32;
    humidity = DHT.humidity;
-   on = 0;
    photoCellReading = analogRead( LIGHT_PIN );
   //Serial.print("light reading: " );
   //Serial.println( photoCellReading );
-  Serial.print("Temp = " );
-  Serial.println( temp );
-  Serial.print( "Temp outside = " );
-  Serial.println( tempOutF );
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
+  //Serial.print("Temp = " );
+  //Serial.println( temp );
+  //Serial.print( "Temp outside = " );
+  //Serial.println( tempOutF );
+  //Serial.print("Humidity = ");
+  //Serial.println(DHT.humidity);
   
   if ( temp > threshold_temperature )
   {
@@ -268,10 +77,11 @@ void read_print_temperature() {
 
     Warning_temperture_low = 0;
      
-    if (heaterindex == 1) {
-     RunningTime = FinishTime - StartTime;
-     TotalRunningTime += RunningTime;
-     heaterindex = 0;
+    if (heaterindex == 1) 
+    {
+      RunningTime = FinishTime - StartTime;
+      TotalRunningTime += RunningTime;
+      heaterindex = 0;
     }
     StartTime = millis();
     
@@ -291,10 +101,11 @@ void read_print_temperature() {
     digitalWrite(in1,LOW);
     
   }
-  Serial.println(TotalRunningTime);
-  */
+ // Serial.println(TotalRunningTime);
+  
 }
-
+*/
+String incomingString;
 
 
 int convertToPercent(int value)
@@ -304,4 +115,217 @@ int convertToPercent(int value)
   if(percentValue > 100)
     percentValue = 100;
   return percentValue;
+}
+void handleRoot() {
+  String html = "<!DOCTYPE html>";
+  html += "<html lang=\"en\">";
+  html += "<head> <meta charset=\"utf-8\"> <meta http-equiv=\"refresh\" content=\"10\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> </head>";
+  html += "<body>";
+  html += "<h1>Sensor Data</h1>";
+  if (Warning_temperture_low) {
+    html += "<p>Warning: temperature is too low</p>";
+  }
+  if (humidity>90) {
+    html += "<p>Warning: humidity is too high</p>";
+  }
+  if (humidity<30) {
+    html += "<p>Warning: humidity is too low</p>";
+  }
+  /*if( percent <10)
+  {
+    html += "<p>Warning: soil moisture is too low</p>";
+  }*/
+  html += "<p>Temperature: </p>";
+  html += temp;
+  html += "<br><br>";
+  html += "<p>Humidity: </p>";
+  html += humidity;
+  html += "<br><br>";
+  html += "<p>Temperature outside</p>";
+  html += tempOutF;
+  //html += "<p>Soil Mositure: </p>";
+  //html += percentString;
+  html += "<br><br>";
+  html += "<p>Light: </p>";
+  html += String(photoCellReading);
+  html += "<br><br>";
+  html += "<h1>Input the threshold temperature</h1>";
+  html += "<input type='text' name='date_hh' id='date_hh' size=2 autofocus> Degree";
+  html += "<div> <br><button id=\"save_button\">Submit</button></div>";
+  html += "<br>";
+  html += "<p>Heater running time(s): </p>";
+  html += TotalRunningTime / 1000;
+  html += "<br><br>";
+  //html += "<div> <br><button onclick="document.getElementById('date_hh').value = ''" id=\"save_button\">Submit</button></div>";
+  html += "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>"; 
+  html += "<script>";
+  html += "var hh;";
+  html += "var mm;";
+  html += "var ss;";
+  html += "$('#save_button').click(function(e){";
+  html += "e.preventDefault();";
+  html += "hh = $('#date_hh').val();";
+  html += "mm = $('#date_mm').val();";
+  html += "ss = $('#date_ss').val();";        
+  html += "$.get('/save?hh=' + hh + '&mm=' + mm + '&ss=' + ss, function(data){";
+  html += "console.log(data);";
+  html += "});";
+  html += "})";     
+  html += "</script>";
+  html += "</body>";
+  html += "</html>";
+
+
+  server.send ( 200, "text/html", html );  
+
+}
+
+
+void handleSave() {
+  if (server.arg("hh")!= ""){
+    threshold_temperature = server.arg("hh").toInt();
+    changeTemp = true;
+  }
+}
+
+
+void setup() {
+
+  pinMode(WRITE_PIN, OUTPUT);
+  digitalWrite(WRITE_PIN, LOW);
+  pinMode(READ_PIN, INPUT);
+  pinMode( LED_PIN, OUTPUT );
+  digitalWrite( LED_PIN, LOW );
+  // Start serial
+  Serial.begin(115200);
+
+  // Connecting to a WiFi network
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  server.on ( "/", handleRoot );
+  server.on ("/save", handleSave);
+  server.begin();
+
+}
+
+String sensorArray[5];
+
+void parseString( String input )
+{
+ int len =  input.length();
+ String current = "";
+ int pos = 0;
+ for( int i = 0; i < len; i++ )
+ {
+  if( isDigit( input.charAt(i) ) || input.charAt(i) == '.' )
+  {
+    current += input.charAt( i );
+  }
+  else if( input.charAt(i) == ';' )
+  {
+    sensorArray[pos] = current;
+    current = "";
+    pos = pos + 1; 
+  }
+ }
+ temp = atof( sensorArray[0].c_str() );
+ tempOutF = atof( sensorArray[1].c_str() );
+ photoCellReading = atof( sensorArray[2].c_str() );
+ humidity = atof( sensorArray[3].c_str() ); 
+}
+
+bool read_val = false;
+unsigned long start_time = 0;
+unsigned long end_time = 0;
+void loop() {
+
+    read_val = digitalRead( READ_PIN );
+    end_time = millis();
+    digitalWrite( WIFI_ON, HIGH );
+    /*
+    if( end_time - start_time > 1000000 )
+    {
+        start_time = end_time;
+        changeTemp = 1;
+    }
+    */
+    if( changeTemp == 1 && !read_val )
+    {
+      digitalWrite( WRITE_PIN, HIGH );
+      delay( 5000 );
+      String tmpString = "TMP";
+      tmpString += threshold_temperature;
+      Serial.print( tmpString );
+      digitalWrite( LED_PIN, LOW );
+      changeTemp = false;
+      digitalWrite( WRITE_PIN, LOW );
+      delay( 5000 );
+    }
+    else if( read_val == 1 )
+    {
+      //digitalWrite( WIFI_ON, HIGH );
+      delay( 100 );
+      while( true )
+      {
+        if (Serial.available() > 0) 
+        {
+        
+          // read the oldest byte in the serial buffer:
+          incomingString = Serial.readString();
+          //check for prepending
+          if( incomingString[0] == 'D' && incomingString[1] == 'T' && incomingString[2] == 'A' )
+          {
+            parseString( incomingString );
+            digitalWrite( LED_PIN, HIGH );
+            break;
+          }
+        delay( 5000 );
+        }
+       
+      }
+    }
+    else
+    { 
+      digitalWrite( LED_PIN, LOW );
+      server.handleClient();
+  //Serial.print("The current threshold temperature is ");
+  //Serial.println(threshold_temperature);
+  //Serial.println("");
+  
+      if (client.connect(thingspeakserver, 80)) //   "184.106.153.149" or api.thingspeak.com
+      {
+  
+          String postStr = apiKey;
+          postStr += "&field1=";
+          postStr += String(temp);   //Temperature
+          postStr += "&field2=";
+          postStr += String(humidity);   //Humidity
+          postStr += "&field3=";
+          postStr += String(tempOutF);   //Humidity
+          postStr += "\r\n\r\n";
+  
+          client.print("POST /update HTTP/1.1\n");
+          client.print("Host: api.thingspeak.com\n");
+          client.print("Connection: close\n");
+          client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+          client.print("Content-Type: application/x-www-form-urlencoded\n");
+          client.print("Content-Length: ");
+          client.print(postStr.length());
+          client.print("\n\n");
+          client.print(postStr);
+      }
+      client.stop();
+
+    //sensorValue = analogRead(Soil_moisture_PIN);
+    //percent = convertToPercent(sensorValue);
+    //percentString = String(percent);
+    //Serial.println( "sensor val" + sensorValue );
+    //Serial.println( "percent: " + percentString );
+
+      delay(5000);
+    }
 }
--- arduino/test_wifi/on_arduino/on_arduino.ino
+++ arduino/test_wifi/on_arduino/on_arduino.ino
@@ -3,7 +3,8 @@ dht DHT;
 #define DHT11_PIN 4 //Nodemcu: D1, Arduino: 4
 #define LIGHT_PIN 3  //Nodemcu: D2, Arduino: 0
 #define OUTSIDE_TEMP_PIN 5  //Nodemcu: D5, Arduino: 1
-#define RW_PIN 10
+#define READ_PIN 10
+#define WRITE_PIN 11
 #define LED_PIN 7
 //#define Soil_moisture_PIN A0  //Nodemcu: D7, Arduino: 1
 int in1 = 6;  //Nodemcu: D6, Arduino: 7
@@ -32,10 +33,14 @@ int stringLength = 0;
 double threshold_temperature = 85;
 bool read_val = 0;
 String toSend = "";
+unsigned long begin_time = 0;
+unsigned long end_time = 0;
 
 void setup() {
 
-  pinMode( RW_PIN, INPUT );
+  pinMode( READ_PIN, INPUT );
+  pinMode( WRITE_PIN, OUTPUT );
+  digitalWrite( WRITE_PIN, LOW );
   pinMode( LED_PIN, OUTPUT );
   digitalWrite( LED_PIN, LOW );
   // Start serial
@@ -43,53 +48,57 @@ void setup() {
 
 }
 String newTemp = "";
+void parseString( String input )
+{
+  String tmp = "";
+  int len = input.length();
+  for( int i = 0; i <  len; i++ )
+  {
+     if( isDigit( input.charAt( i ) ) || input.charAt( i ) == '.' )
+     {
+      tmp += input.charAt( i );
+     }
+  }
+  threshold_temperature = atof( tmp.c_str() );
+}
 void loop() {
 
-  read_val = digitalRead( RW_PIN );
-
+  read_val = digitalRead( READ_PIN );
+  end_time = millis();
   if( read_val == 1 )
   {
+       digitalWrite( WRITE_PIN, LOW );
       //digitalWrite( LED_PIN, HIGH ); 
       delay( 100 );
-      if( Serial.available() > 0 )
+      while( true )
       {
-       newTemp = Serial.readString();
-       Serial.println( newTemp );
-       if ( newTemp == "hello" )
-       {
-        Serial.println("yay");
-        digitalWrite( LED_PIN, HIGH ); 
-        delay( 5000 );
-       }
+        if( Serial.available() > 0 )
+        {
+         newTemp = Serial.readString();
+         if( newTemp[0] == 'T' && newTemp[1] == 'M' && newTemp[2] == 'P' )
+         {
+          digitalWrite( LED_PIN, HIGH ); 
+          parseString( newTemp );
+          break;
+         }
+        }
       }
   }
   
-  else
+  else 
   {
-    digitalWrite( LED_PIN, LOW );
-
-    Serial.print( "arduino" );
-    delay( 1000 );
-    /*
+    delay( 5000 );
     toSend = "";
     out_reading = analogRead( OUTSIDE_TEMP_PIN );
     voltage = out_reading * 3.3;
     voltage /= 1024.0;
   
-    tempOutF = ( voltage - 0.5 ) * 100 * 9.0/5.0 + 25;
+    tempOutF = ( voltage - 0.5 ) * 100 * 9.0/5.0;
     chk = DHT.read11( DHT11_PIN );
     temp = DHT.temperature * 1.8 + 32;
     humidity = DHT.humidity;
     photoCellReading = analogRead( LIGHT_PIN );
-  
-    toSend += temp;
-    toSend += ";";
-    toSend += tempOutF;
-    toSend += ";";
-    toSend += photoCellReading;
-    toSend += ";";
-  
-    digitalWrite( in1, HIGH );
+
     if ( temp > threshold_temperature )
     {
       //if temperature is above threshold temperature, turn relay off and turn LED off
@@ -97,19 +106,18 @@ void loop() {
   
       Warning_temperture_low = 0;
        
-      if (heaterindex == 1) {
-       RunningTime = FinishTime - StartTime;
-       TotalRunningTime += RunningTime;
-       heaterindex = 0;
+      if (heaterindex == 1) 
+      {
+        RunningTime = FinishTime - StartTime;
+        TotalRunningTime += RunningTime;
+        heaterindex = 0;
       }
       StartTime = millis();
-      
     }
+    
     else if ( temp <= threshold_temperature )
     {
       //if temperature is below threshold temperature, turn on relay and turn LED on
-      
-  
       if (temp + 20 < threshold_temperature)
           Warning_temperture_low = 1;
       else  
@@ -117,11 +125,30 @@ void loop() {
       
       heaterindex = 1;
       FinishTime = millis();
-      digitalWrite(in1,LOW);
-      
+      digitalWrite(in1,LOW);   
     }
-    Serial.println( toSend );
-    delay( 5000 );
-    digitalWrite( in1, LOW );*/
+
+    bool check = digitalRead( READ_PIN );
+    if( end_time - begin_time > 10000 && !check )
+    {
+      begin_time = end_time;
+      digitalWrite( LED_PIN, LOW );
+      digitalWrite( WRITE_PIN, HIGH );
+      delay( 5000 );
+      toSend += "DTA";
+      toSend += temp;
+      toSend += ";";
+      toSend += tempOutF;
+      toSend += ";";
+      toSend += photoCellReading;
+      toSend += ";";
+      toSend += humidity;
+      toSend += ";";
+  
+      Serial.print( toSend );
+      //Serial.print( toSend );
+      digitalWrite( WRITE_PIN, LOW );
+      delay( 5000 );
+    }
   }
 }
