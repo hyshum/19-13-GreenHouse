4020debff2ecd57d326675b7fdd13cba0b833207
Qian Zhang <z727629029@gmail.com>
2019-03-30 17:23:10 -0400

    Updated

--- Lambda/3-27-3_Warnning_Message(final).js
+++ Lambda/3-27-3_Warnning_Message(final).js
@@ -14,6 +14,7 @@
 
 console.log('Loading function');
 var AWS = require("aws-sdk");
+const request = require('request');
 var highest;
 var Lowest;
 //Readfrom DB
@@ -259,4 +260,24 @@ function Send_Warning_Message(IOT_data,Highest_temperature,Lowest_temperature,co
     if (index == 1) {
         sns.publish(params, context.done);
     }
+}
+
+function Switch_On() {
+    console.log('Running Switch');
+    const option = 'https://maker.ifttt.com/trigger/ec464greenhouseON/with/key/cvJYmevJ910Cxw7Zr5Y6Ac';
+    request(option, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+      });
+}
+
+function Switch_OFF() {
+    console.log('Running Switch');
+    const option = 'https://maker.ifttt.com/trigger/ec464greenhouseOFF/with/key/cvJYmevJ910Cxw7Zr5Y6Ac';
+    request(option, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+      });
 }
